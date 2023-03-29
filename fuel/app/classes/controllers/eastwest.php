4a1931920c949c0b46dbf0c1a6fbc3f0dec107da
Quintencrum <quintencrum@gmail.com>
2023-03-28 18:08:56 -0600

    adding vars

--- fuel/app/classes/controllers/eastwest.php
+++ fuel/app/classes/controllers/eastwest.php
@@ -1,13 +1,15 @@
 <?php
 class Controller_eastwest extends Controller_Template {
 
-    public $direction = "";
-    public $direction2 = "";
+    public $numRowsColumns = "";    //number of rows/columns
+    public $numColors = "";         //number of colors
 
-public function action_index(){
+public function action_index(){ //Probably need to RENAME
     $data = array();
-    if(isset($_GET['direction'])){
-        $dir = $_GET['direction'];
+    if(isset($_GET['numRowsColumns']) & isset($_GET['numColors'])){
+        $rows = $_GET['numRowsColumns'];
+        $cols = $_GET['numColors'];
+
         if($dir == "east"){
             $this->template->title= 'East Home Page';
             $this->template->css = "east.css";
