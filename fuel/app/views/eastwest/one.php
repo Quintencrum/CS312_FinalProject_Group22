e34f6004dc5c43df7b1822eae902eafab1cce4fd
Quintencrum <quintencrum@gmail.com>
2023-03-29 13:29:34 -0600

    adding previous assingment fuel information

---
+++ fuel/app/views/eastwest/one.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body class="container, center">
+        <p class="colors">
+            This is East Page 1!
+        </p>
+        <br>
+        <!-- <img class="center" src="images.png" > -->
+        <br>
+        <br>
+        <br>
+        <br>
+        <p class="greenText">
+            There is supposed to be two seperate paragraphs for this assignment. I am not sure why because I have thing very interesting to say for my homepage. Perhaps for the second paragraph I will come up with something fun to say! But for now I will leave you with the quote of the day:
+            <br>
+            "Where are my pants?"-Q
+        </p>    
+        <br>
+        <br>
+        <br>
+    </body>
+    <!-- <footer class="container, center">
+        <img src="csMeme.jpeg" class="center">
+        <br>
+        <br>
+        <h4 class="center">
+            This homepage was created for CS312!
+        </h4>
+
+    </footer> -->
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
+    east image url:
+
+    https://www.google.com/imgres?imgurl=https%3A%2F%2Fetc.usf.edu%2Fclipart%2F74400%2F74419%2F74419_145_m3-2_b_lg.gif&imgrefurl=https%3A%2F%2Fetc.usf.edu%2Fclipart%2F74400%2F74419%2F74419_145_m3-2_b.htm&tbnid=EJ87-RbTeWFF1M&vet=12ahUKEwiLo9Sio_X8AhWcx8kDHZwACcwQMygregUIARDDAg..i&docid=8Tbz8NU0lHVChM&w=1024&h=516&q=east&ved=2ahUKEwiLo9Sio_X8AhWcx8kDHZwACcwQMygregUIARDDAg
+-->
