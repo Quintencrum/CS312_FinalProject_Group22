4383d5933125b7f1d121440def7243ee0d3bb15d
Joseph Erickson <112026033+Son-Of-Erick@users.noreply.github.com>
2023-03-30 18:59:41 -0600

    Update template.php

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -1,8 +1,8 @@
 <head>
+        <title><?php echo $title ?></title>
         <meta charset="utf-8">
-        <title><?php $title ?></title>
         <meta name="author" content="CS312">
-        <meta name="description" content="A homepage for lab 2">
+        <meta name="description" content="312 Group Project">
         <meta name="keywords" content="Final Project, HTML5, Group 22, CS312, CSU, Colorado State University">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
@@ -12,7 +12,7 @@
 <body>
     <nav>
         <ul>
-            <li><a href="home.php">Home</a></li>
+            <li><a href = "https://cs.colostate.edu:4444/~eid/m1/">Home</a></li>
             <li><a href="about.php">About</a></li>
             <li><a href="color_coordinate_generation.php">Color Coordinate Generation</a></li>
         </ul>
