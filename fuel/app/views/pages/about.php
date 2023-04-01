e842e0a44f47e1f8df54e42b718cc385b52cbd95
Garrett Schleich <gschleic@colostate.edu>
2023-03-31 21:04:43 -0600

    Garrett Style Changes

--- fuel/app/views/pages/about.php
+++ fuel/app/views/pages/about.php
@@ -1,29 +1,35 @@
 <!DOCTYPE html>
 <html>
-    <header>
     <h1>Team 22</h1>
     <h2>About the Team:</h2>
-    </html>
-    <header>
-
     <body>
         <h3>Quinten Crum</h3>
-        <?php echo Asset::img("quintenCrum_Image.jpeg")?>
+        <div id="team">
+        <?php echo Asset::img("quintenCrum_Image.png")?>
+        </div>
         <p>About: <br>
             Quinten is a Senior at Colorado State University with a major in Data Science and a concentration in Economics. He enjoys travling to new places as you can see with the image above. <br>
             <strong>He also loves dogs!</strong>
         </p>
 
         <h3>Joseph Erickson</h3>
-        <?php echo Asset::img("joe-profile-pic.jpeg")?>
-        <p>About: My name is Joseph, I am a Senior here at Colorado State University majoring in Computer Science. I love to golf, fish, and do anything outdoors. Just like Quinten, <strong>I love dogs!</strong></p>
+        <div id="team2">
+        <?php echo Asset::img("joe-profile-pic.png")?>
+        </div>
+        <p>About: <br>
+            My name is Joseph, I am a Senior here at Colorado State University majoring in Computer Science. I love to golf, fish, and do anything outdoors. Just like Quinten, <strong>I love dogs!</strong></p> <br>
 
         <h3>Garrett Schleich</h3>
-        <?php echo Asset::img("garrett_image.jpg")?>
-        <p>About: My name is Garrett Schleich and I'm a Colorado native from Aurora. I'm currently a senior majoring in general Computer Science with a Certificate in Entrepreneurship. One of my goals is to start developing apps and software outside of school. I hope to graduate and pursue more real-world developing skills. I used to be a member of the CSU League of Legends team, and I previously studied abroad in South Korea.</p>
+        <div id="team">
+        <?php echo Asset::img("garrett_image.png")?>
+        </div>
+        <p>About: <br>
+            My name is Garrett Schleich and I'm a Colorado native from Aurora. I'm currently a senior majoring in general Computer Science with a Certificate in Entrepreneurship. One of my goals is to start developing apps and software outside of school. I hope to graduate and pursue more real-world developing skills. I used to be a member of the CSU League of Legends team, and I previously studied abroad in South Korea.</p> <br>
 
         <h3>Nicholas Eto</h3>
-        <?php echo Asset::img("Eto-image.jpg")?>
-        <p>About: <br> My name is Nicholas Eto, I am a senior at Colorado State University majoring in General Computer Science. I am from Littleton and I enjoy fishing, traveling, and cooking/eating.</p>
+        <div id="team">
+        <?php echo Asset::img("Eto-image.png")?>
+        </div>
+        <p>About: <br> My name is Nicholas Eto, I am a senior at Colorado State University majoring in General Computer Science. I am from Littleton and I enjoy fishing, traveling, and cooking/eating.</p> <br>
     </body>
 </html>
