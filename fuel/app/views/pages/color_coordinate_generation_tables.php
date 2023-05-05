16b616d015d85ef648226eb30f9afa129ccc9460
Joseph Erickson <112026033+Son-Of-Erick@users.noreply.github.com>
2023-05-05 15:19:08 -0600

    Update and rename color_coordinate_generation.php to color_coordinate_generation_tables.php

---
+++ fuel/app/views/pages/color_coordinate_generation_tables.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <h1>Team 22</h1>
+        <h2>Color Coordinate Generation:</h2>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <header>
+    <body>
+        <p>
+            
+        </p>
+    </body>
+</html>
