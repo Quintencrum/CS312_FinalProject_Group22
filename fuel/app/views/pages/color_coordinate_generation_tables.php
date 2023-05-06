a8c88f943e7ae702a0927c8d13e41136d5745901
Garrett Schleich <111801278+gschleich@users.noreply.github.com>
2023-05-05 21:46:40 -0600

    Update color_coordinate_generation_tables.php

--- fuel/app/views/pages/color_coordinate_generation_tables.php
+++ fuel/app/views/pages/color_coordinate_generation_tables.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
-    <header>
-        <h1>Team 22</h1>
-        <h2>Color Coordinate Generation:</h2>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <header>
-    <body>
-        <p>
-            
-        </p>
-    </body>
+  <head>
+    <title>My Page</title>
+  </head>
+  <body>
+    <div id="navspacing">
+      <h1>Team 22</h1>
+      <h2>Color Coordinate Generation:</h2>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </body>
 </html>
