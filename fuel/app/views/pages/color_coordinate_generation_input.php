bc8b5f9ca584c1928a009d6c269a845084564137
Garrett Schleich <111801278+gschleich@users.noreply.github.com>
2023-05-05 21:44:58 -0600

    Update color_coordinate_generation_input.php

--- fuel/app/views/pages/color_coordinate_generation_input.php
+++ fuel/app/views/pages/color_coordinate_generation_input.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html>
-    <header>
+<div id="navspacing">
         <h1>Team 22</h1>
         <h2>Color Coordinate Generation:</h2>
-    <header>
     <body>
-        <?php echo Form::open(['action' => 'index.php/milestone1/tables', 'method' => 'get']); ?>
+        <?php echo Form::open(['action' => 'index.php/m1/tables', 'method' => 'get']); ?>
         <?php echo Form::label('Enter a number of rows/columns:', 'val1'); ?>
         <?php echo Form::input('val1'); ?>
         <?php echo Form::label('Enter a number of colors:', 'val2'); ?>
@@ -13,4 +12,5 @@
         <?php echo Form::submit('submit', 'Submit'); ?>
         <?php echo Form::close(); ?>
     </body>
+</div>
 </html>
